952868b54a81f6ce9d2a0f7fea32f68bc5ca60fa
erick <erick.8bld@gmail.com>
2020-11-17 12:50:03 +0200

    fixed possible deadlock occurence in _print functions in src/vga_buffer.rs and src/serial.rs

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -3,8 +3,25 @@ use x86_64::structures::idt::{
     InterruptStackFrame
 };
 use lazy_static::lazy_static;
-use crate::println;
+use pic8259_simple::ChainedPics;
+use spin;
+use crate::{
+    println,
+    print
+};
 use crate::gdt;
+
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+
+pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(
+    unsafe {
+        ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET)
+    }
+);
+
 
 lazy_static!{
     static ref IDT: InterruptDescriptorTable = {
@@ -16,6 +33,8 @@ lazy_static!{
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+
         // returning the IDT
         idt
     };
@@ -31,6 +50,23 @@ fn test_breakpoint_exception() {
 }
 
 
+#[derive(Clone, Copy, Debug)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        return self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        return usize::from(self.as_u8())
+    }
+}
+
+
 /* Exception Handlers */
 
 extern "x86-interrupt" fn breakpoint_handler(
@@ -43,4 +79,16 @@ extern "x86-interrupt" fn double_fault_handler(
     stack_frame: &mut InterruptStackFrame, _error_code: u64
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT (UNHANDLED EXCEPTION)\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(
+    _stack_frame: &mut InterruptStackFrame
+) {
+    print!(".");
+
+    unsafe {
+        PICS.lock().notify_end_of_interrupt(
+            InterruptIndex::Timer.as_u8()
+        );
+    };
 }
